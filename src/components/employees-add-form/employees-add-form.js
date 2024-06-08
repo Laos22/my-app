826df04150f53f3169e7106745f86fb405b85174
Laos22 <olega1987@gmail.com>
2024-06-08 14:47:27 +0300

    onChange form

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,20 +1,46 @@
 import './employees-add-form.css';
+import { Component } from 'react';
 
-function EmployeesAddForm() {
-    return (
-        <div className="app-add-form">
-            <h3>Добавьте нового сотрудника</h3>
-            <form className="add-form d-flex">
-                <input type="text"
-                    className="form-control new-post-lable"
-                    placeholder="Как его зовут?" />
-                <input type="number"
-                    className="form-control new-post-lable"
-                    placeholder="З/П в $?" />
-                <button type="submit" className="btn btn-outline-light" >Добавить</button>
-            </form>
-        </div>
-    )
+class EmployeesAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            salary: ''
+        }
+    }
+
+    onChangeValue = (e) => {
+       this.setState({
+        [e.target.name] : e.target.value
+       })
+
+    }
+
+    render() {
+
+        const {name, salary} = this.state;
+        return (
+            <div className="app-add-form">
+                <h3>Добавьте нового сотрудника</h3>
+                <form className="add-form d-flex">
+                    <input type="text"
+                        className="form-control new-post-lable"
+                        placeholder="Как его зовут?"
+                        onChange={this.onChangeValue}
+                        name='name'
+                        value={name} />
+                    <input type="number"
+                        className="form-control new-post-lable"
+                        placeholder="З/П в $?" 
+                        onChange={this.onChangeValue}
+                        name='salary'
+                        value={salary} />
+                    <button type="submit" className="btn btn-outline-light" >Добавить</button>
+                </form>
+            </div>
+        )
+    } 
 }
 
 export default EmployeesAddForm;
